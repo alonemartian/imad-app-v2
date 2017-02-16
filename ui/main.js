c46308046fa3b2a7ba001c6ff1099544760691d2
alonemartian <devashiskhandelwal@gmail.com>
2017-02-16 20:47:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,24 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
-
-
-//change the content of main-text div
-var element=document.getElementById('main-text');
-element.innerHTML ='New Value';
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft=marginLeft= 1;
-    img.style.marginLeft= marginLeft + 'px';
-}
-
-img.onclick = function () {
-    var interval =setInterval(moveRight,50);
+button.onclick = function(){
     
+    //create request to the counter endpoint
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState ===XMLHttpRequest.DONE){
+            if(request.status=== 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString()''
+            }
+        }
+    };
+    //make request
+    request.open('GET','http://alonemartian.imad.hasura-app.io/counter',true);
+    request.send(null);
+   
 };
